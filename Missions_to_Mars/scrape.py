9a5965083ee49b52d2949c4dcec983ae73c11761
Maria Quinton <maria.quinton28@gmail.com>
2020-12-05 18:59:24 -0500

    Updates

--- Missions_to_Mars/scrape.py
+++ Missions_to_Mars/scrape.py
@@ -13,4 +13,15 @@ def init_browser():
 def scrape():
     #Tiles
     browser = init_browser()
-    
+    mars_titles = {}
+
+    # News Urls
+    url = 'https://mars.nasa.gov/news/'
+    browser.visit(url)
+    time.sleep(2)
+
+    html = browser.mars_html
+    soup = BeautifulSoup(html, 'html.parser')
+
+    mars_titles['news_title'] = soup.find('div', class_="content_title").get_text()
+    mars_titles["news_snip"] = soup.find('div', class_="rollover_description_inner").get_text()
